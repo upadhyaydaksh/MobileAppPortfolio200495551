eb55290f108546dd738c18ed58e663d9ad9b576c
Daksh <upadhyaydaksh96@gmail.com>
2022-03-23 01:56:03 -0400

    notifications changes.

--- PitchVenture/PitchVenture/AppDelegates/AppDelegate.swift
+++ PitchVenture/PitchVenture/AppDelegates/AppDelegate.swift
@@ -26,21 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         //Push Notifications
-        if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().delegate = self
 
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
-        } else {
-          let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
-
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+        
         application.registerForRemoteNotifications()
 
         Messaging.messaging().delegate = self
